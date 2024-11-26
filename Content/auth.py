af8a2e137421887af5e48948133b85ab762de20f
RjDeVerra <55550662+RjDeVerra@users.noreply.github.com>
2024-11-26 19:19:38 +0300

    Update auth.py

--- Content/auth.py
+++ Content/auth.py
@@ -1,1 +1,20 @@
+import requests
 
+token = 'your_personal_access_token'
+
+base_url = 'https://api.github.com/'
+
+headers = {'Authorization': f'token {token}'}
+
+def search_repositories(query):
+    url = f"{base_url}search/repositories?q={query}&sort=stars&order=desc"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+data = search_repositories('machine learning')
+if data:
+    print(data)
